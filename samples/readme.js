13d4715df7557ab0096920f4fbb9d2389fbfcd3b
Mo Wang <mo.hy.wang@gmail.com>
2011-11-02 22:11:59 -0700

    add samples

---
+++ samples/readme.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    var body = document.getElementsByTagName("body")[0];
+    var p = document.createElement("p");
+    p.innerHTML = "*** Made by Mo Wang ***";
+    body.appendChild(p);  
+};
